795167a561746ea69e820eb183411e30ee27c66d
ValueHeavy <82959043+ValueHeavy@users.noreply.github.com>
2025-02-25 19:17:45 +0100

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactFormMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'message' => 'required',
+        ]);
+
+        Mail::to('ccccd13c29-2cfc48@inbox.mailtrap.io')->send(new ContactFormMail());
+    }
+}
